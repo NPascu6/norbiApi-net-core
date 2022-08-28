372edd04ae6033c8d9d8e53a157d6062703eb3b4
Norbert Pascu <58431719+NPascu6@users.noreply.github.com>
2022-08-28 14:30:42 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,30 @@
 using Microsoft.EntityFrameworkCore;
 using NPascuAPI.Contexts;
-
 var builder = WebApplication.CreateBuilder(args);
+
+var defaultConnectionString = string.Empty;
+
+if (builder.Environment.EnvironmentName == "Development")
+{
+    defaultConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+}
+else
+{
+    // Use connection string provided at runtime by Heroku.
+    var connectionUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+    connectionUrl = connectionUrl.Replace("postgres://", string.Empty);
+    var userPassSide = connectionUrl.Split("@")[0];
+    var hostSide = connectionUrl.Split("@")[1];
+
+    var user = userPassSide.Split(":")[0];
+    var password = userPassSide.Split(":")[1];
+    var host = hostSide.Split("/")[0];
+    var database = hostSide.Split("/")[1].Split("?")[0];
+
+    defaultConnectionString = $"Host={host};Database={database};Username={user};Password={password};SSL Mode=Require;Trust Server Certificate=true";
+}
+
 
 var configurationBuilder = new ConfigurationBuilder()
                             .SetBasePath(builder.Environment.ContentRootPath)
@@ -11,7 +34,6 @@ var configurationBuilder = new ConfigurationBuilder()
 
 builder.Configuration.AddConfiguration(configurationBuilder.Build());
 
-var defaultConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<UserContext>(options =>
    options.UseNpgsql(defaultConnectionString));
 
@@ -23,6 +45,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
